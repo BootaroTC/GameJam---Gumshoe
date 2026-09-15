ebce8cf9ddcfd44b6b9607f35e361fbdfecd0993
NokiVok68 <szebasztiankralik11@gmail.com>
2026-09-15 22:51:40 +0100

    Test

---
+++ Scripts/PlayerScript/player.gd
@@ -0,0 +1,37 @@
+extends CharacterBody3D
+
+
+@export var SPEED = 5.0
+@export var mouse_sens:float = 0.01
+
+@onready var neck: Node3D = $Neck
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var mouse_motion:Vector2 = event.relative
+		rotate_y(-(mouse_motion.x * mouse_sens))
+		neck.rotate_x(-(mouse_motion.y * mouse_sens))
+		neck.rotation.x = deg_to_rad(clamp(rad_to_deg(neck.rotation.x), -90, 50))
+
+func movement(delta):
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	var input_dir := Input.get_vector("left", "right", "forward", "back")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+
+func _physics_process(delta: float) -> void:
+	movement(delta)
