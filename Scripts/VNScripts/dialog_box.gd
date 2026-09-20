34e849c23af825be440a225ce2a0fb8cb6630fb9
BootaroTC <tina.grandieri@gmail.com>
2026-09-20 02:35:06 +0100

    VN Improvements

--- Scripts/VNScripts/dialog_box.gd
+++ Scripts/VNScripts/dialog_box.gd
@@ -1,11 +1,18 @@
 extends Control
 
 signal text_animation_done
+signal choice_selected
+#signal sentence_pause
+
+# Preload Choice BUtton Scene
+const ChoiceButtonScene = preload("res://Scene/VNScenes/ReusableAssets/player_choice.tscn")
 
 @onready var dialog_line: RichTextLabel = $DialogBox/DialogLine
 @onready var speaker_name: Label = $PanelContainer/SpeakerName
+@onready var choice_list =  %ChoiceList
 @onready var text_blip_sound: AudioStreamPlayer2D = %TextBlipSound
 @onready var text_blip_timer: Timer = %TextBlipTimer
+@onready var sentence_pause_timer: Timer = %SentencePauseTimer
 
 
 const ANIMATION_SPEED : int = 30
@@ -17,17 +24,29 @@ var current_character_details : Dictionary
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Hide choices initially
+	choice_list.hide()
+	
 	text_blip_timer.timeout.connect(_on_text_blip_timeout)
+	sentence_pause_timer.timeout.connect(_on_sentence_pause_timeout)
 	
 func _process(delta: float) -> void:
-	if animate_text:
+	if animate_text and sentence_pause_timer.is_stopped():
 		if dialog_line.visible_ratio < 1:
 			dialog_line.visible_ratio += (1.0/dialog_line.text.length()) * (ANIMATION_SPEED * delta)
 			if dialog_line.visible_characters > current_visible_caracters:
 				current_visible_caracters = dialog_line.visible_characters
+				var current_char = dialog_line.text[current_visible_caracters - 1]
+				if current_visible_caracters < dialog_line.text.length():
+					var next_char = dialog_line.text[current_visible_caracters]
+					if NO_SOUND_CHARS.has(current_char) and next_char == " ":
+						text_blip_timer.stop()
+						sentence_pause_timer.start()
+						#sentence_pause.emit(sentence_pause_timer.wait_time)
 
 		else:
 			animate_text = false
+			text_blip_timer.stop()
 			text_animation_done.emit()
 
 func change_line(character_name: Character.Name, line : String):
@@ -38,9 +57,33 @@ func change_line(character_name: Character.Name, line : String):
 	dialog_line.text = line
 	animate_text = true
 	text_blip_timer.start()
+	
+func display_choices(choices: Array):
+	# Clear existing choices
+	for child in choice_list.get_children():
+		child.queue_free()
+		
+	# Create a new button for each choice
+	for choice in choices:
+		var choice_button = ChoiceButtonScene.instantiate()
+		choice_button.text = choice["text"]
+		# Attach signal to button
+		choice_button.pressed.connect(_on_choice_button_pressed.bind(choice["goto"]))
+		# Add to choice list
+		choice_list.add_child(choice_button)
+	
+	# Show Choice list
+	choice_list.show()
 
 func skip_text_animation():
 	dialog_line.visible_ratio = 1
 
 func _on_text_blip_timeout():
 	text_blip_sound.play_sound(current_character_details)
+
+func _on_sentence_pause_timeout():
+	text_blip_timer.start()
+
+func _on_choice_button_pressed(anchor: String):
+	choice_selected.emit(anchor)
+	choice_list.hide()
--- Scripts/VNScripts/main_vn_scene.gd
+++ Scripts/VNScripts/main_vn_scene.gd
@@ -7,23 +7,24 @@ extends Node2D
 # to tell script what point in the code its at
 var dialog_index : int = 0
 
-const dialog_lines : Array[String] = [
-	"Goole: Yo, I think we lowk gotta kill this guy.",
-	"BadGuy: I'm gonna friiigggin ERASE you noob!",
-	"Goole: Heh... Guess I've gotta use it at last... ban.....kai...",
-	"BadGuy: *locks in* [shake]SEGUNDA ETAPA[/shake]!!"
-]
+var dialog_lines : Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#load dialog
+	dialog_lines = load_dialog("res://Sprites/VNAssets/Story/Story.json")
 	#connect signal
 	dialog_ui.text_animation_done.connect(_on_text_animation_done)
+	dialog_ui.choice_selected.connect(_on_choice_selected)
+	#dialog_ui.sentence_pause.connect(_on_sentence_pause)
 	#process first line of dialog
 	dialog_index = 0
 	process_current_line()
 
 func _input(event):
-	if event.is_action_pressed("next_line"):
+	var line = dialog_lines[dialog_index]
+	var has_choices = line.has("choices")
+	if event.is_action_pressed("next_line") and not has_choices:
 		if dialog_ui.animate_text:
 			dialog_ui.skip_text_animation()
 		else:
@@ -31,20 +32,69 @@ func _input(event):
 				dialog_index += 1
 				process_current_line()
 			
-func parse_line(line: String):
-	var line_info = line.split(":")
-	assert(len(line_info) >= 2) #idk I saw the guy do this but I think it just returns an error if the line length is less than 2 so just bug checking
-	return {
-		"speaker_name": line_info[0],
-		"dialog_line": line_info[1]
-	}
 
 func process_current_line():
 	var line = dialog_lines[dialog_index]
-	var line_info = parse_line(line)
-	var character_name = Character.get_enum_from_string(line_info["speaker_name"])
-	dialog_ui.change_line(character_name, line_info["dialog_line"])
-	character_sprite.change_character(character_name)
+	# Check if this is goto command
+	if line.has("goto"):
+		dialog_index = get_anchor_position(line["goto"])
+		process_current_line()
+		return
+	
+	# Check if this is just an anchor declaration
+	if line.has("anchor"):
+		dialog_index += 1
+		process_current_line()
+		return
+		
+	# Check Choices
+	if line.has("choices"):
+		# Display Choices
+		dialog_ui.display_choices(line["choices"])
+	else:
+		# Reading Dialog
+		var character_name = Character.get_enum_from_string(line["speaker"])
+		dialog_ui.change_line(character_name, line["text"])
+		character_sprite.change_character(character_name)
+
+func get_anchor_position(anchor: String):
+	# Find the anchor with matching name
+	for i in range(dialog_lines.size()):
+		if dialog_lines[i].has("anchor") and dialog_lines[i]["anchor"] == anchor:
+			return i
+			
+	# Anchor wasnt found here
+	print("Error: could not find anchor: " + anchor)
+	return null
+
+func load_dialog(file_path):
+	# check if file exist
+	if not FileAccess.file_exists(file_path):
+		print("Error: File does not exist: " + file_path)
+		return null
+		
+	# Open the file
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	if file == null:
+		print("Error: Failed to open file: " + file_path)
+		return null
+		
+	# Read as text
+	var content = file.get_as_text()
+	
+	# Parse Json
+	var json_content = JSON.parse_string(content)
+	
+	# Check if Parsin was successful
+	if json_content == null:
+		print("Error: Failed to parse JSON from file: " + file_path)
+		return null
+	
+	return json_content
 
 func _on_text_animation_done():
 	character_sprite.play_idle_animation()
+
+func _on_choice_selected(anchor: String):
+	dialog_index = get_anchor_position(anchor)
+	process_current_line()
